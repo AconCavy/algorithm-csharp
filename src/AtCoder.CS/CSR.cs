7da17d809869ddccef3c33c40ca580393ade3081
AconCavy <aconcavy@gmail.com>
2020-09-08 05:48:24 +0900

    Add SCCGraph

---
+++ src/AtCoder.CS/CSR.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AtCoder.CS
+{
+    public class CSR<T>
+    {
+        public int[] Start { get; }
+
+        public T[] Edges { get; }
+
+        public CSR(int n, IEnumerable<(int, T)> edges)
+        {
+            Start = new int[n + 1];
+            var tuples = edges as (int, T)[] ?? edges.ToArray();
+            Edges = new T[tuples.Length];
+            foreach (var e in tuples)
+            {
+                Start[e.Item1 + 1]++;
+            }
+
+            for (var i = 0; i < n; i++)
+            {
+                Start[i + 1] += Start[i];
+            }
+
+            foreach (var (i, t) in tuples)
+            {
+                Edges[Start[i]++] = t;
+            }
+        }
+    }
+}
---
+++ src/AtCoder.CS/SCCGraph.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AtCoder.CS
+{
+    public class SCCGraph
+    {
+        private struct Edge
+        {
+            public int To;
+        }
+
+        private readonly int _n;
+        private readonly List<(int, Edge)> _edges;
+
+        public SCCGraph(int n = 0)
+        {
+            _n = n;
+            _edges = new List<(int, Edge)>();
+        }
+
+        public void AddEdge(int from, int to)
+        {
+            if (from < 0 || _n <= from) throw new ArgumentException(nameof(from));
+            if (to < 0 || _n <= to) throw new ArgumentException(nameof(to));
+            _edges.Add((from, new Edge {To = to}));
+        }
+
+        public (int, IEnumerable<int>) Ids()
+        {
+            var g = new CSR<Edge>(_n, _edges);
+            var (nowOrd, groupNum) = (0, 0);
+            var visited = new Stack<int>(_n);
+            var low = new int[_n];
+            var ord = new int[_n];
+            var ids = new int[_n];
+
+            void DFS(int v)
+            {
+                low[v] = ord[v] = nowOrd++;
+                visited.Push(v);
+                for (var i = g.Start[v]; i < g.Start[v + 1]; i++)
+                {
+                    var to = g.Edges[i].To;
+                    if (ord[to] == -1)
+                    {
+                        DFS(to);
+                        low[v] = System.Math.Min(low[v], low[to]);
+                    }
+                    else
+                    {
+                        low[v] = System.Math.Min(low[v], ord[to]);
+                    }
+                }
+
+                if (low[v] != ord[v]) return;
+                while (true)
+                {
+                    var u = visited.Pop();
+                    ord[u] = _n;
+                    ids[u] = groupNum;
+                    if (u == v) break;
+                }
+
+                groupNum++;
+            }
+
+            for (var i = 0; i < _n; i++)
+            {
+                if (ord[i] == -1) DFS(i);
+            }
+
+            for (var i = 0; i < _n; i++)
+            {
+                ids[i] = groupNum - 1 - ids[i];
+            }
+
+            return (groupNum, ids);
+        }
+    }
+}
