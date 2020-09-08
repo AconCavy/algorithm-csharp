e6de96d0548b66b8af2b0ffe65e454b84c0cf731
AconCavy <aconcavy@gmail.com>
2020-09-08 22:58:57 +0900

    Add PriorityQueue

---
+++ src/AtCoder.CS/PriorityQueue.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AtCoder.CS
+{
+    public class PriorityQueue<T> : IEnumerable<T>
+    {
+        private readonly SortedDictionary<T, int> _data;
+
+        public PriorityQueue(int count, IComparer<T> comparer = null)
+            : this(new List<T>(count), comparer)
+        {
+        }
+
+        public PriorityQueue(IEnumerable<T> data = null, IComparer<T> comparer = null)
+        {
+            comparer ??= Comparer<T>.Default;
+            _data = data == null
+                ? new SortedDictionary<T, int>(comparer)
+                : new SortedDictionary<T, int>(data.ToDictionary(x => x, _ => 1), comparer);
+        }
+
+        public void Enqueue(T item)
+        {
+            if (!_data.ContainsKey(item)) _data[item] = 0;
+            _data[item]++;
+        }
+
+        public T Dequeue()
+        {
+            if (!_data.Any()) throw new InvalidOperationException();
+            var key = Peek();
+            if (_data[key] > 1) _data[key]--;
+            else _data.Remove(key);
+            return key;
+        }
+
+        public bool TryDequeue(out T result)
+        {
+            if (_data.Any())
+            {
+                result = Dequeue();
+                return true;
+            }
+
+            result = default;
+            return false;
+        }
+
+        public bool TryPeek(out T result)
+        {
+            if (_data.Any())
+            {
+                result = Peek();
+                return true;
+            }
+
+            result = default;
+            return false;
+        }
+
+        public T Peek()
+        {
+            if (!_data.Any()) throw new InvalidOperationException();
+            using var e = _data.GetEnumerator();
+            e.MoveNext();
+            var (ret, _) = e.Current;
+            return ret;
+        }
+
+        public void Clear() => _data.Clear();
+        public bool Contains(T item) => _data.ContainsKey(item);
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (array == null) throw new ArgumentNullException(nameof(array));
+            if (arrayIndex < 0) throw new ArgumentOutOfRangeException(nameof(arrayIndex));
+            GetFlatData().ToList().CopyTo(array);
+        }
+
+        public IEnumerator<T> GetEnumerator() => GetFlatData().GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        private IEnumerable<T> GetFlatData() => _data.SelectMany(x => Enumerable.Repeat(x.Key, x.Value));
+    }
+}
