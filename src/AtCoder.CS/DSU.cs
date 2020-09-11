a7ebbffbd5c236fbb23eaa05517a135aba786d4b
AconCavy <aconcavy@gmail.com>
2020-09-12 03:09:29 +0900

    Modify DSU and add its tests

--- src/AtCoder.CS/DSU.cs
+++ src/AtCoder.CS/DSU.cs
@@ -4,12 +4,6 @@ using System.Linq;
 
 namespace AtCoder.CS
 {
-    /// <summary>
-    /// Implement (union by size) + (path compression)
-    /// Reference:
-    /// Zvi Galil and Giuseppe F. Italiano,
-    /// Data structures and algorithms for disjoint set union problems
-    /// </summary>
     public class DSU
     {
         private readonly int _n;
@@ -23,8 +17,8 @@ namespace AtCoder.CS
 
         public int Merge(int a, int b)
         {
-            if (a < 0 || _n <= a) throw new ArgumentException(nameof(a));
-            if (b < 0 || _n <= b) throw new ArgumentException(nameof(b));
+            if (a < 0 || _n <= a) throw new IndexOutOfRangeException(nameof(a));
+            if (b < 0 || _n <= b) throw new IndexOutOfRangeException(nameof(b));
             var (x, y) = (LeaderOf(a), LeaderOf(b));
             if (x == y) return x;
             if (-_parentOrSize[x] < -_parentOrSize[y]) (x, y) = (y, x);
@@ -35,21 +29,21 @@ namespace AtCoder.CS
 
         public bool IsSame(int a, int b)
         {
-            if (a < 0 || _n <= a) throw new ArgumentException(nameof(a));
-            if (b < 0 || _n <= b) throw new ArgumentException(nameof(b));
+            if (a < 0 || _n <= a) throw new IndexOutOfRangeException(nameof(a));
+            if (b < 0 || _n <= b) throw new IndexOutOfRangeException(nameof(b));
             return LeaderOf(a) == LeaderOf(b);
         }
 
         public int LeaderOf(int a)
         {
-            if (a < 0 || _n <= a) throw new ArgumentException(nameof(a));
+            if (a < 0 || _n <= a) throw new IndexOutOfRangeException(nameof(a));
             if (_parentOrSize[a] < 0) return a;
             return _parentOrSize[a] = LeaderOf(_parentOrSize[a]);
         }
 
         public int SizeOf(int a)
         {
-            if (a < 0 || _n <= a) throw new ArgumentException(nameof(a));
+            if (a < 0 || _n <= a) throw new IndexOutOfRangeException(nameof(a));
             return -_parentOrSize[LeaderOf(a)];
         }
 
@@ -64,11 +58,7 @@ namespace AtCoder.CS
             }
 
             var ret = new List<int>[_n].Select(x => new List<int>()).ToArray();
-            for (var i = 0; i < _n; i++)
-            {
-                ret[leaders[i]].Add(i);
-            }
-
+            for (var i = 0; i < _n; i++) ret[leaders[i]].Add(i);
             return ret.Where(x => x.Any());
         }
     }
---
+++ tests/AtCoder.CS.Tests/DSUTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace AtCoder.CS.Tests
+{
+    public class DSUTests
+    {
+        [Test]
+        public void SimpleTest()
+        {
+            var dsu = new DSU(2);
+            var x = dsu.Merge(0, 1);
+            Assert.That(dsu.LeaderOf(0), Is.EqualTo(x));
+            Assert.That(dsu.LeaderOf(1), Is.EqualTo(x));
+            Assert.That(dsu.IsSame(0, 1), Is.True);
+            Assert.That(dsu.SizeOf(0), Is.EqualTo(2));
+        }
+
+        [Test]
+        public void LineTest()
+        {
+            const int n = 500000;
+            var dsu = new DSU(n);
+            for (var i = 0; i < n - 1; i++)
+                dsu.Merge(i, i + 1);
+            Assert.That(dsu.SizeOf(0), Is.EqualTo(n));
+            Assert.That(dsu.Groups().Count(), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void LineReverseTest()
+        {
+            const int n = 500000;
+            var dsu = new DSU(n);
+            for (var i = n - 2; i >= 0; i--)
+                dsu.Merge(i, i + 1);
+            Assert.That(dsu.SizeOf(0), Is.EqualTo(n));
+            Assert.That(dsu.Groups().Count(), Is.EqualTo(1));
+        }
+    }
+}
