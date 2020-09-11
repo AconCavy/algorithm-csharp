4bc7932b00a28484ff6b7e8e3c5b06c0d97b35d8
AconCavy <aconcavy@gmail.com>
2020-09-11 21:31:35 +0900

    Modify MInt

--- src/AtCoder.CS/MInt.cs
+++ src/AtCoder.CS/MInt.cs
@@ -3,30 +3,36 @@
     public readonly struct MInt
     {
         public long Value => _data;
-        private const int Mod = 998244353;
+        public static int Mod => 998244353;
         private readonly long _data;
         public MInt(long data) => _data = (0 <= data ? data : data + Mod) % Mod;
         public static implicit operator long(MInt mint) => mint._data;
         public static implicit operator MInt(long val) => new MInt(val);
-        public static MInt operator +(MInt a, MInt b) => new MInt(a._data + b._data);
+        public static MInt operator +(MInt a, MInt b) => a._data + b._data;
         public static MInt operator +(MInt a, long b) => a._data + b;
-        public static MInt operator -(MInt a, MInt b) => new MInt(a._data - b._data);
+        public static MInt operator -(MInt a, MInt b) => a._data - b._data;
         public static MInt operator -(MInt a, long b) => a._data - b;
-        public static MInt operator *(MInt a, MInt b) => new MInt(a._data * b._data);
+        public static MInt operator *(MInt a, MInt b) => a._data * b._data;
         public static MInt operator *(MInt a, long b) => a._data * (b % Mod);
-        public static MInt operator /(MInt a, MInt b) => new MInt(a._data * GetInv(b));
+        public static MInt operator /(MInt a, MInt b) => a._data * GetInv(b);
+        public static MInt operator /(MInt a, long b) => a._data * GetInv(b);
+
         public static bool operator ==(MInt a, MInt b) => a._data == b._data;
         public static bool operator !=(MInt a, MInt b) => a._data != b._data;
+        public static MInt operator &(MInt a, MInt b) => a._data & b._data;
+        public static MInt operator &(MInt a, long b) => a._data & b;
+        public static MInt operator |(MInt a, MInt b) => a._data | b._data;
+        public static MInt operator |(MInt a, long b) => a._data | b;
         public override bool Equals(object obj) => _data.Equals(obj);
         public override int GetHashCode() => _data.GetHashCode();
         public override string ToString() => _data.ToString();
 
-        private static long GetInv(long a)
+        public static long GetInv(long a)
         {
             a %= Mod;
             if (a < 0) a += Mod;
             if (a == 0) return 0;
-            var (s, t, m0, m1) = ((long)Mod, a, 0L, 1L);
+            var (s, t, m0, m1) = ((long) Mod, a, 0L, 1L);
             while (t > 1)
             {
                 var u = s / t;
