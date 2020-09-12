fcbcc782675e142db5460fe43a84962981ff86a6
AconCavy <aconcavy@gmail.com>
2020-09-13 01:29:08 +0900

    Modify MinCostFlowGraph

--- src/AtCoder.CS/MinCostFlowGraph.cs
+++ src/AtCoder.CS/MinCostFlowGraph.cs
@@ -40,24 +40,42 @@ namespace AtCoder.CS
             }
         }
 
-        private int _n;
-        private List<InternalEdge>[] _edges;
-        private List<(int X, int Y)> _pos;
+        private readonly struct Q : IComparable<Q>
+        {
+            public readonly long Key;
+            public readonly int To;
+
+            public Q(long key, int to)
+            {
+                Key = key;
+                To = to;
+            }
+
+            public int CompareTo(Q other)
+            {
+                var ret = Comparer<long>.Default.Compare(Key, other.Key);
+                if (ret == 0) ret = Comparer<int>.Default.Compare(To, other.To);
+                return ret;
+            }
+        }
+
+        private readonly int _n;
+        private readonly List<InternalEdge>[] _edges;
+        private readonly List<(int X, int Y)> _pos;
 
         public MinCostFlowGraph(int n = 0)
         {
             _n = n;
-            _edges = new List<InternalEdge>[n];
+            _edges = new List<InternalEdge>[n].Select(x => new List<InternalEdge>()).ToArray();
             _pos = new List<(int X, int Y)>();
         }
 
         public int AddEdge(int from, int to, long cap, long cost)
         {
-            if (from < 0 || _n <= from) throw new ArgumentException(nameof(from));
-            if (to < 0 || _n <= to) throw new ArgumentException(nameof(to));
-            if (cap < 0) throw new ArgumentException(nameof(cap));
+            if (from < 0 || _n <= from) throw new IndexOutOfRangeException(nameof(from));
+            if (to < 0 || _n <= to) throw new IndexOutOfRangeException(nameof(to));
             var m = _pos.Count;
-            _pos.Add((from, _edges.Length));
+            _pos.Add((from, _edges[from].Count));
             _edges[from].Add(new InternalEdge(to, _edges[to].Count, cap, cost));
             _edges[to].Add(new InternalEdge(from, _edges[from].Count - 1, 0, -cost));
             return m;
@@ -65,8 +83,7 @@ namespace AtCoder.CS
 
         public Edge GetEdge(int i)
         {
-            if (i < 0 || _n <= i) throw new ArgumentException(nameof(i));
-            var m = _pos.Count;
+            if (i < 0 || _pos.Count <= i) throw new IndexOutOfRangeException(nameof(i));
             var e = _edges[_pos[i].X][_pos[i].Y];
             var re = _edges[e.To][e.Rev];
             return new Edge(_pos[i].X, e.To, e.Cap + re.Cap, re.Cap, e.Cost);
@@ -79,44 +96,45 @@ namespace AtCoder.CS
 
         public (long, long) FlowOf(int s, int t) => FlowOf(s, t, long.MaxValue);
 
-        public (long, long) FlowOf(int s, int t, long flowLimit) => SlopeOf(s, t, flowLimit).Last();
+        public (long, long) FlowOf(int s, int t, long flowLimit) => SlopeOf(s, t, flowLimit).First();
 
         public IEnumerable<(long, long)> SlopeOf(int s, int t) => SlopeOf(s, t, long.MaxValue);
 
         public IEnumerable<(long, long)> SlopeOf(int s, int t, long flowLimit)
         {
-            if (s < 0 || _n <= s) throw new ArgumentException(nameof(s));
-            if (t < 0 || _n <= t) throw new ArgumentException(nameof(t));
+            if (s < 0 || _n <= s) throw new IndexOutOfRangeException(nameof(s));
+            if (t < 0 || _n <= t) throw new IndexOutOfRangeException(nameof(t));
             if (s == t) throw new ArgumentException();
             var dual = new long[_n];
-            var visited = new bool[_n];
             int[] pv;
             int[] pe;
 
-            bool DualRef()
+            bool BFS()
             {
                 var dist = Enumerable.Repeat(long.MaxValue, _n).ToArray();
+                var visited = new bool[_n];
                 pv = Enumerable.Repeat(-1, _n).ToArray();
                 pe = Enumerable.Repeat(-1, _n).ToArray();
-                var queue = new PriorityQueue<(long Key, int To)>();
+                var queue = new PriorityQueue<Q>();
                 dist[s] = 0;
-                queue.Enqueue((0, s));
+                queue.Enqueue(new Q(0, s));
                 while (queue.Any())
                 {
-                    var v = queue.Dequeue().To;
+                    var cur = queue.Dequeue();
+                    var v = cur.To;
                     if (visited[v]) continue;
                     visited[v] = true;
                     if (v == t) break;
                     for (var i = 0; i < _edges[v].Count; i++)
                     {
                         var e = _edges[v][i];
-                        if (visited[e.To] || e.Cap == 0) continue;
+                        if (visited[e.To] || e.Cap <= 0) continue;
                         var c = e.Cost - dual[e.To] + dual[v];
                         if (dist[e.To] <= dist[v] + c) continue;
                         dist[e.To] = dist[v] + c;
                         pv[e.To] = v;
                         pe[e.To] = i;
-                        queue.Enqueue((dist[e.To], e.To));
+                        queue.Enqueue(new Q(dist[e.To], e.To));
                     }
                 }
 
@@ -133,9 +151,8 @@ namespace AtCoder.CS
             var (flow, cost, prev) = (0L, 0L, -1L);
             var ret = new Stack<(long, long)>();
             ret.Push((flow, cost));
-            while (flow < flowLimit)
+            while (flow < flowLimit && BFS())
             {
-                if (!DualRef()) break;
                 var c = flowLimit - flow;
                 for (var v = t; v != s; v = pv[v])
                 {
