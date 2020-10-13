3cece909e20d3a2d0815265e3d948d73c85ebfcf
AconCavy <aconcavy@gmail.com>
2020-10-13 22:49:01 +0900

    Modify field names in TwoSatisfiability.

--- src/AtCoderLibraryCSharp/TwoSatisfiability.cs
+++ src/AtCoderLibraryCSharp/TwoSatisfiability.cs
@@ -7,32 +7,32 @@ namespace AtCoderLibraryCSharp
     {
         public bool[] Answer { get; }
 
-        private readonly int _n;
+        private readonly int _length;
         private readonly StronglyConnectedComponent _scc;
 
-        public TwoSatisfiability(int n = 0)
+        public TwoSatisfiability(int length = 0)
         {
-            _n = n;
-            Answer = new bool[n];
-            _scc = new StronglyConnectedComponent(2 * n);
+            _length = length;
+            Answer = new bool[length];
+            _scc = new StronglyConnectedComponent(length * 2);
         }
 
         public void AddClause(int i, bool f, int j, bool g)
         {
-            if (i < 0 || _n < i) throw new IndexOutOfRangeException(nameof(i));
-            if (j < 0 || _n < j) throw new IndexOutOfRangeException(nameof(j));
-            _scc.AddEdge(2 * i + (f ? 0 : 1), 2 * j + (g ? 1 : 0));
-            _scc.AddEdge(2 * j + (g ? 0 : 1), 2 * i + (f ? 1 : 0));
+            if (i < 0 || _length < i) throw new IndexOutOfRangeException(nameof(i));
+            if (j < 0 || _length < j) throw new IndexOutOfRangeException(nameof(j));
+            _scc.AddEdge(i * 2 + (f ? 0 : 1), j * 2 + (g ? 1 : 0));
+            _scc.AddEdge(j * 2 + (g ? 0 : 1), i * 2 + (f ? 1 : 0));
         }
 
         public bool IsSatisfiable()
         {
-            var (_, tmp) = _scc.GetIds();
-            var ids = tmp.ToArray();
-            for (var i = 0; i < _n; i++)
+            var (_, identities) = _scc.GetIds();
+            var ids = identities.ToArray();
+            for (var i = 0; i < _length; i++)
             {
-                if (ids[2 * i] == ids[2 * i + 1]) return false;
-                Answer[i] = ids[2 * i] < ids[2 * i + 1];
+                if (ids[i * 2] == ids[i * 2 + 1]) return false;
+                Answer[i] = ids[i * 2] < ids[i * 2 + 1];
             }
 
             return true;
