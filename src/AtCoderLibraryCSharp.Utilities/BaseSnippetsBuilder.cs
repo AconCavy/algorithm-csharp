05bf5056d7b96a963522cbf7e2610f1f05773ef7
AconCavy <aconcavy@gmail.com>
2020-10-13 23:38:46 +0900

    Modify field name and remove unnecessary processing in BaseSnippetsBuilder

--- src/AtCoderLibraryCSharp.Utilities/BaseSnippetsBuilder.cs
+++ src/AtCoderLibraryCSharp.Utilities/BaseSnippetsBuilder.cs
@@ -8,26 +8,22 @@ namespace AtCoderLibraryCSharp.Utilities
     {
         protected string[] FilePaths { get; }
         protected abstract string Extension { get; }
-        private readonly string _outputPath;
+        private readonly string _outputDirectory;
         private readonly string _outputName;
 
         protected BaseSnippetsBuilder(string outputName, string targetDirectory, string outputDirectory = null)
         {
             _outputName = outputName;
             targetDirectory = targetDirectory.Replace(@"\", @"/");
-            if (targetDirectory[^1] != '/') targetDirectory += "/";
-            if (!Directory.Exists(targetDirectory))
-                throw new DirectoryNotFoundException(nameof(targetDirectory));
+            if (!Directory.Exists(targetDirectory)) throw new DirectoryNotFoundException(nameof(targetDirectory));
             FilePaths = Directory.GetFiles(targetDirectory, "*.cs");
-
-            _outputPath = outputDirectory ?? @"./outputs/";
-            if (_outputPath[^1] != '/') _outputPath += "/";
-            if (!Directory.Exists(_outputPath)) Directory.CreateDirectory(_outputPath);
+            _outputDirectory = outputDirectory ?? @"./outputs/";
+            if (!Directory.Exists(_outputDirectory)) Directory.CreateDirectory(_outputDirectory);
         }
 
         public async ValueTask BuildAsync(CancellationToken cancellationToken = default)
         {
-            var outputPath = Path.Combine(_outputPath, _outputName + Extension);
+            var outputPath = Path.Combine(_outputDirectory, _outputName + Extension);
             await using var fileStream = File.Exists(outputPath)
                 ? new FileStream(outputPath, FileMode.Truncate)
                 : File.Create(outputPath);
