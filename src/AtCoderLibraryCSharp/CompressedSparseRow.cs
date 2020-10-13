4bcdf30ca66f13f7da5a1f9586d8c28097cc5b87
AconCavy <aconcavy@gmail.com>
2020-10-13 23:09:17 +0900

    Modify field names in CompressedSparseRow

--- src/AtCoderLibraryCSharp/CompressedSparseRow.cs
+++ src/AtCoderLibraryCSharp/CompressedSparseRow.cs
@@ -8,13 +8,13 @@ namespace AtCoderLibraryCSharp
         public int[] Start { get; }
         public T[] Edges { get; }
 
-        public CompressedSparseRow(int n, IEnumerable<(int, T)> edges)
+        public CompressedSparseRow(int length, IEnumerable<(int, T)> edges)
         {
-            Start = new int[n + 1];
+            Start = new int[length + 1];
             var es = edges.ToArray();
             Edges = new T[es.Length];
             foreach (var e in es) Start[e.Item1 + 1]++;
-            for (var i = 0; i < n; i++) Start[i + 1] += Start[i];
+            for (var i = 0; i < length; i++) Start[i + 1] += Start[i];
             var counter = Start.ToArray();
             foreach (var (i, t) in es) Edges[counter[i]++] = t;
         }
