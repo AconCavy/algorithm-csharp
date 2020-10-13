90e1f668c0d13ddce7c5fdd39d222e965594877e
AconCavy <aconcavy@gmail.com>
2020-10-14 01:25:45 +0900

    Add LowestCommonAncestor and tests

---
+++ src/AtCoderLibraryCSharp/LowestCommonAncestor.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AtCoderLibraryCSharp
+{
+    public class LowestCommonAncestor
+    {
+        private readonly int _length;
+        private readonly int _log;
+        private readonly int[] _depth;
+        private readonly int[][] _parents;
+
+        public LowestCommonAncestor(IEnumerable<IEnumerable<int>> tree, int root = 0)
+        {
+            var T = tree.Select(x => x.ToArray()).ToArray();
+            _length = T.Length;
+            if (root < 0 || _length <= root) throw new IndexOutOfRangeException(nameof(root));
+            while (_length >> _log > 0) _log++;
+            _depth = new int[_length];
+            _parents = new int[_length][].Select(x => new int[_log]).ToArray();
+            var queue = new Queue<(int current, int previous, int distance)>();
+            queue.Enqueue((root, -1, 0));
+            var used = new bool[_length];
+            used[root] = true;
+            while (queue.Any())
+            {
+                var (current, previous, distance) = queue.Dequeue();
+                _parents[current][0] = previous;
+                _depth[current] = distance;
+                foreach (var next in T[current].Where(next => !used[next]))
+                {
+                    used[next] = true;
+                    queue.Enqueue((next, current, distance + 1));
+                }
+            }
+
+            for (var i = 0; i + 1 < _log; i++)
+            {
+                for (var v = 0; v < _length; v++)
+                {
+                    var parent = _parents[v][i];
+                    _parents[v][i + 1] = parent == -1 ? -1 : _parents[parent][i];
+                }
+            }
+        }
+
+        public int Find(int u, int v)
+        {
+            if (u < 0 || _length <= u) throw new IndexOutOfRangeException(nameof(u));
+            if (v < 0 || _length <= v) throw new IndexOutOfRangeException(nameof(v));
+            if (_depth[u] > _depth[v]) (u, v) = (v, u);
+            v = GetAncestor(v, _depth[v] - _depth[u]);
+            if (u == v) return u;
+            for (var i = _log - 1; i >= 0; i--)
+                if (_parents[u][i] != _parents[v][i])
+                    (u, v) = (_parents[u][i], _parents[v][i]);
+
+            return _parents[u][0];
+        }
+
+        public int GetAncestor(int v, int height)
+        {
+            if (v < 0 || _length <= v) throw new IndexOutOfRangeException(nameof(v));
+            var parent = v;
+            for (var i = 0; i < _log && parent != -1; i++)
+                if ((height >> i & 1) == 1)
+                    parent = _parents[parent][i];
+
+            return parent;
+        }
+
+        public int GetDistance(int u, int v)
+        {
+            if (u < 0 || _length <= u) throw new IndexOutOfRangeException(nameof(u));
+            if (v < 0 || _length <= v) throw new IndexOutOfRangeException(nameof(v));
+            var p = Find(u, v);
+            return _depth[u] + _depth[v] - _depth[p] * 2;
+        }
+    }
+}
---
+++ tests/AtCoderLibraryCSharp.Tests/LowestCommonAncestorTests.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AtCoderLibraryCSharp.Tests
+{
+    public class LowestCommonAncestorTests
+    {
+        [Test]
+        public void InitializeTest()
+        {
+            var tree = new[] {new[] {1}, new int[0]};
+            Assert.DoesNotThrow(() => _ = new LowestCommonAncestor(tree));
+
+            tree = new[] {new[] {1}, new[] {0}};
+            Assert.DoesNotThrow(() => _ = new LowestCommonAncestor(tree));
+        }
+
+        [Test]
+        public void FindTest()
+        {
+            var tree = new[]
+            {
+                new[] {1, 2},
+                new[] {0, 3, 4}, new[] {0, 5},
+                new[] {1}, new[] {1}, new[] {2}
+            };
+            var lca = new LowestCommonAncestor(tree);
+            Assert.That(lca.Find(0, 1), Is.EqualTo(0));
+            Assert.That(lca.Find(1, 2), Is.EqualTo(0));
+            Assert.That(lca.Find(3, 5), Is.EqualTo(0));
+            Assert.That(lca.Find(1, 3), Is.EqualTo(1));
+            Assert.That(lca.Find(1, 4), Is.EqualTo(1));
+            Assert.That(lca.Find(3, 4), Is.EqualTo(1));
+            Assert.That(lca.Find(4, 3), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void GetAncestorTest()
+        {
+            var tree = new[]
+            {
+                new[] {1, 2},
+                new[] {0, 3, 4}, new[] {0, 5},
+                new[] {1}, new[] {1}, new[] {2}
+            };
+            var lca = new LowestCommonAncestor(tree);
+            Assert.That(lca.GetAncestor(0, 0), Is.EqualTo(0));
+            Assert.That(lca.GetAncestor(0, 3), Is.EqualTo(-1));
+            Assert.That(lca.GetAncestor(1, 1), Is.EqualTo(0));
+            Assert.That(lca.GetAncestor(3, 1), Is.EqualTo(1));
+            Assert.That(lca.GetAncestor(3, 2), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void GetDistanceTest()
+        {
+            var tree = new[]
+            {
+                new[] {1, 2},
+                new[] {0, 3, 4}, new[] {0, 5},
+                new[] {1}, new[] {1}, new[] {2}
+            };
+            var lca = new LowestCommonAncestor(tree);
+            Assert.That(lca.GetDistance(0, 1), Is.EqualTo(1));
+            Assert.That(lca.GetDistance(0, 2), Is.EqualTo(1));
+            Assert.That(lca.GetDistance(0, 3), Is.EqualTo(2));
+            Assert.That(lca.GetDistance(0, 4), Is.EqualTo(2));
+            Assert.That(lca.GetDistance(0, 5), Is.EqualTo(2));
+            Assert.That(lca.GetDistance(3, 0), Is.EqualTo(2));
+            Assert.That(lca.GetDistance(3, 2), Is.EqualTo(3));
+            Assert.That(lca.GetDistance(3, 5), Is.EqualTo(4));
+        }
+
+        [Test]
+        public void RootNot0Test()
+        {
+            var tree = new[]
+            {
+                new[] {1, 2},
+                new[] {0, 3, 4}, new[] {0, 5},
+                new[] {1}, new[] {1}, new[] {2}
+            };
+            var lca = new LowestCommonAncestor(tree, 2);
+            Assert.That(lca.Find(0, 5), Is.EqualTo(2));
+            Assert.That(lca.Find(1, 5), Is.EqualTo(2));
+            Assert.That(lca.Find(3, 5), Is.EqualTo(2));
+            Assert.That(lca.GetAncestor(0, 0), Is.EqualTo(0));
+            Assert.That(lca.GetAncestor(0, 3), Is.EqualTo(-1));
+            Assert.That(lca.GetAncestor(5, 3), Is.EqualTo(-1));
+            Assert.That(lca.GetAncestor(3, 3), Is.EqualTo(2));
+            Assert.That(lca.GetAncestor(3, 2), Is.EqualTo(0));
+            Assert.That(lca.GetDistance(0, 1), Is.EqualTo(1));
+            Assert.That(lca.GetDistance(0, 2), Is.EqualTo(1));
+            Assert.That(lca.GetDistance(0, 3), Is.EqualTo(2));
+            Assert.That(lca.GetDistance(0, 4), Is.EqualTo(2));
+            Assert.That(lca.GetDistance(0, 5), Is.EqualTo(2));
+            Assert.That(lca.GetDistance(3, 0), Is.EqualTo(2));
+            Assert.That(lca.GetDistance(3, 2), Is.EqualTo(3));
+            Assert.That(lca.GetDistance(3, 5), Is.EqualTo(4));
+        }
+
+        [Test]
+        public void OneWayTreeTest()
+        {
+            var tree = new[]
+            {
+                new[] {1, 2},
+                new[] {3, 4}, new[] {5},
+                new int[0], new int[0], new int[0]
+            };
+            var lca = new LowestCommonAncestor(tree);
+            Assert.That(lca.Find(0, 1), Is.EqualTo(0));
+            Assert.That(lca.Find(1, 2), Is.EqualTo(0));
+            Assert.That(lca.Find(3, 5), Is.EqualTo(0));
+            Assert.That(lca.Find(1, 3), Is.EqualTo(1));
+            Assert.That(lca.Find(1, 4), Is.EqualTo(1));
+            Assert.That(lca.Find(3, 4), Is.EqualTo(1));
+            Assert.That(lca.Find(4, 3), Is.EqualTo(1));
+            Assert.That(lca.GetAncestor(0, 0), Is.EqualTo(0));
+            Assert.That(lca.GetAncestor(0, 3), Is.EqualTo(-1));
+            Assert.That(lca.GetAncestor(1, 1), Is.EqualTo(0));
+            Assert.That(lca.GetAncestor(3, 1), Is.EqualTo(1));
+            Assert.That(lca.GetAncestor(3, 2), Is.EqualTo(0));
+            Assert.That(lca.GetDistance(0, 1), Is.EqualTo(1));
+            Assert.That(lca.GetDistance(0, 2), Is.EqualTo(1));
+            Assert.That(lca.GetDistance(0, 3), Is.EqualTo(2));
+            Assert.That(lca.GetDistance(0, 4), Is.EqualTo(2));
+            Assert.That(lca.GetDistance(0, 5), Is.EqualTo(2));
+            Assert.That(lca.GetDistance(3, 0), Is.EqualTo(2));
+            Assert.That(lca.GetDistance(3, 2), Is.EqualTo(3));
+            Assert.That(lca.GetDistance(3, 5), Is.EqualTo(4));
+        }
+
+        [Test]
+        public void InvalidArgumentsTest()
+        {
+            var tree = new[] {new[] {1}, new int[0]};
+            Assert.Throws<IndexOutOfRangeException>(() => _ = new LowestCommonAncestor(tree, -1));
+            Assert.Throws<IndexOutOfRangeException>(() => _ = new LowestCommonAncestor(tree, 2));
+
+            tree = new[] {new[] {1}, new[] {0}};
+            Assert.Throws<IndexOutOfRangeException>(() => _ = new LowestCommonAncestor(tree, -1));
+            Assert.Throws<IndexOutOfRangeException>(() => _ = new LowestCommonAncestor(tree, 2));
+
+            tree = new[]
+            {
+                new[] {1, 2},
+                new[] {3, 4}, new[] {5},
+                new int[0], new int[0], new int[0]
+            };
+            var lca = new LowestCommonAncestor(tree);
+            Assert.Throws<IndexOutOfRangeException>(() => _ = lca.Find(-1, 0));
+            Assert.Throws<IndexOutOfRangeException>(() => _ = lca.Find(6, 0));
+            Assert.Throws<IndexOutOfRangeException>(() => _ = lca.Find(0, -1));
+            Assert.Throws<IndexOutOfRangeException>(() => _ = lca.Find(0, 6));
+
+            Assert.Throws<IndexOutOfRangeException>(() => _ = lca.GetAncestor(-1, 0));
+            Assert.Throws<IndexOutOfRangeException>(() => _ = lca.GetAncestor(6, 0));
+
+            Assert.Throws<IndexOutOfRangeException>(() => _ = lca.GetDistance(-1, 0));
+            Assert.Throws<IndexOutOfRangeException>(() => _ = lca.GetDistance(6, 0));
+            Assert.Throws<IndexOutOfRangeException>(() => _ = lca.GetDistance(0, -1));
+            Assert.Throws<IndexOutOfRangeException>(() => _ = lca.GetDistance(0, 6));
+        }
+    }
+}
