a507ef98da14f9668719433c59b6c605ce8d0979
AconCavy <aconcavy@gmail.com>
2020-10-28 00:24:10 +0900

    Modify ModuloInteger to implement IEquatable and change arguments in some methods

--- src/AtCoderLibraryCSharp/ModuloInteger.cs
+++ src/AtCoderLibraryCSharp/ModuloInteger.cs
@@ -2,7 +2,7 @@
 
 namespace AtCoderLibraryCSharp
 {
-    public readonly struct ModuloInteger
+    public readonly struct ModuloInteger : IEquatable<ModuloInteger>
     {
         public long Value { get; }
         public static long Modulo { get; private set; } = 998244353;
@@ -15,16 +15,16 @@ namespace AtCoderLibraryCSharp
         // public const long Modulo = 998244353;
 
         public ModuloInteger(long value) => Value = 0 <= value % Modulo ? value % Modulo : value % Modulo + Modulo;
-        public static implicit operator long(ModuloInteger mint) => mint.Value;
         public static implicit operator int(ModuloInteger mint) => (int) mint.Value;
-        public static implicit operator ModuloInteger(long value) => new ModuloInteger(value);
+        public static implicit operator long(ModuloInteger mint) => mint.Value;
         public static implicit operator ModuloInteger(int value) => new ModuloInteger(value);
-        public static ModuloInteger operator +(ModuloInteger a, ModuloInteger b) => a.Value + b.Value;
-        public static ModuloInteger operator -(ModuloInteger a, ModuloInteger b) => a.Value - b.Value;
-        public static ModuloInteger operator *(ModuloInteger a, ModuloInteger b) => a.Value * b.Value;
-        public static ModuloInteger operator /(ModuloInteger a, ModuloInteger b) => a * b.Inverse();
-        public static bool operator ==(ModuloInteger a, ModuloInteger b) => a.Value == b.Value;
-        public static bool operator !=(ModuloInteger a, ModuloInteger b) => a.Value != b.Value;
+        public static implicit operator ModuloInteger(long value) => new ModuloInteger(value);
+        public static ModuloInteger operator +(in ModuloInteger a, in ModuloInteger b) => a.Value + b.Value;
+        public static ModuloInteger operator -(in ModuloInteger a, in ModuloInteger b) => a.Value - b.Value;
+        public static ModuloInteger operator *(in ModuloInteger a, in ModuloInteger b) => a.Value * b.Value;
+        public static ModuloInteger operator /(in ModuloInteger a, in ModuloInteger b) => a * b.Inverse();
+        public static bool operator ==(in ModuloInteger a, in ModuloInteger b) => a.Value == b.Value;
+        public static bool operator !=(in ModuloInteger a, in ModuloInteger b) => a.Value != b.Value;
         public bool Equals(ModuloInteger other) => Value == other.Value;
         public override bool Equals(object obj) => obj is ModuloInteger other && Equals(other);
         public override int GetHashCode() => Value.GetHashCode();
@@ -51,14 +51,14 @@ namespace AtCoderLibraryCSharp
 
         public ModuloInteger Power(long n) => Power(Value, n);
 
-        public static ModuloInteger Power(ModuloInteger value, long n)
+        public static ModuloInteger Power(long value, long n)
         {
             if (n < 0) throw new ArgumentException();
-            var ret = new ModuloInteger(1);
+            var ret = 1L;
             while (n > 0)
             {
-                if ((n & 1) > 0) ret *= value;
-                value *= value;
+                if ((n & 1) > 0) ret = ret * value % Modulo;
+                value = value * value % Modulo;
                 n >>= 1;
             }
 
