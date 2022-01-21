5b9953ff5facfae41bc5ef7fb318681eda15e490
AconCavy <aconcavy@gmail.com>
2022-01-21 23:34:31 +0900

    Modify CompressedSparseRow

--- src/Algorithm/CompressedSparseRow.cs
+++ src/Algorithm/CompressedSparseRow.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -5,14 +6,16 @@ namespace Algorithm
 {
     public class CompressedSparseRow<T>
     {
-        public CompressedSparseRow(int length, IEnumerable<(int, T)> edges)
+        public CompressedSparseRow(int length, IEnumerable<(int ID, T)> edges)
         {
             Start = new int[length + 1];
             var es = edges.ToArray();
             Edges = new T[es.Length];
-            foreach (var e in es) Start[e.Item1 + 1]++;
+            foreach (var e in es) Start[e.ID + 1]++;
             for (var i = 0; i < length; i++) Start[i + 1] += Start[i];
-            var counter = Start.ToArray();
+
+            var counter = new int[length + 1];
+            Start.AsSpan().CopyTo(counter.AsSpan());
             foreach (var (i, t) in es) Edges[counter[i]++] = t;
         }
 
